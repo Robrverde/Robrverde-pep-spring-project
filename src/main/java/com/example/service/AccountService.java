869668848fa38cdaa45c444ff8ef1a4803baf725
Roberto Edde <robrverde2000@gmail.com>
2024-03-09 18:58:25 +0000

    Implemented dependency injection and basic implementation for register method to create a new account

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,7 +1,29 @@
 package com.example.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.entity.Account;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+    
+    private List<Account> accountList = new ArrayList<>();
+
+    private MessageService messageService;
+
+    //Dependency injection of Message Service
+    @Autowired
+    public AccountService(MessageService messageService)
+    {
+        this.messageService = messageService;
+    }
+
+    public void register(Account newAccount)
+    {
+        accountList.add(newAccount);
+    }
 }
