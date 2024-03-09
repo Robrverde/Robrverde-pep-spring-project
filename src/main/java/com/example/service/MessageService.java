dfef3552e1bd95fe9fb9cb77a140dec33a539d8f
Roberto Edde <robrverde2000@gmail.com>
2024-03-09 18:49:37 +0000

    Added Service annotation for the AccountService and MessageService classes

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,16 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.entity.Message;
+
+
+@Service
 public class MessageService {
+    private List<Message> messageList = new ArrayList<>();
+
+    
 }
