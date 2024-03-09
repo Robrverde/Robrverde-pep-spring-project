b4abe8a0e40bf332e158e767d7e9927d73b44421
Roberto Edde <robrverde2000@gmail.com>
2024-03-09 19:37:47 +0000

    Implemented custom exception class

---
+++ src/main/java/com/example/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.exception;
+
+public class ResourceNotFoundException extends RuntimeException
+{
+    
+    public ResourceNotFoundException(String message)
+    {
+        super(message);
+    }
+}
