0f2e6cf823da1fae20a2307d2f54febae2d5ff2e
Roberto Edde <robrverde2000@gmail.com>
2024-03-09 19:34:51 +0000

    Implemented basic functionality for getMessageByID, deleteMessageByID, patchMessageByID
    Fully created a method to return the messageList

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -6,11 +6,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.entity.Message;
+import com.example.exception.ResourceNotFoundException;
 
 
 @Service
 public class MessageService {
     private List<Message> messageList = new ArrayList<>();
 
-    
+    public List<Message> getMessageList()
+    {
+        return messageList;
+    }
+
+    //GET localhost:8080/messages/{message_id}
+    public Message getMessageByID(int message_id) throws ResourceNotFoundException
+    {
+        for(Message message : messageList)
+        {
+            if(message.getMessage_id().equals(message_id))
+                return message;
+        }
+
+        throw new ResourceNotFoundException("Message not found");
+    }
+
+    //DELETE localhost:8080/messages/{message_id}
+    public void deleteMessageByID(int message_id)
+    {
+        messageList.removeIf(message -> message.getMessage_id().equals(message_id));
+    }
+
+    //PATCH localhost:8080/messages/{message_id}
+    public void patchMessageByID(int message_id, String new_text) throws ResourceNotFoundException
+    {
+        for(Message message : messageList)
+        {   
+            //check if message exists
+            if(message.getMessage_id().equals(message_id))
+            {
+                //check if the new text is valid (not blank and not over 255 characters)
+                if(new_text.length() > 0 && new_text.length() <= 255)
+                    message.setMessage_text(new_text);
+                
+                return;
+            }
+        }
+
+        throw new ResourceNotFoundException("Message not found");
+    }
 }
