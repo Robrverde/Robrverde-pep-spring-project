5de9b1524e4b641fa2250f45d3b3a6d1aab0df9c
Roberto Edde <robrverde2000@gmail.com>
2024-03-16 02:52:25 +0000

    Removed commented code and unused imports

--- src/main/java/com/example/exception/ExceptionAndErrorController.java
+++ src/main/java/com/example/exception/ExceptionAndErrorController.java
@@ -3,7 +3,6 @@ package com.example.exception;
 import javax.naming.AuthenticationException;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,15 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAndErrorController
 {
-
-    /* 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleMissingParam(MissingServletRequestParameterException ex)
-    {
-        return ex.getParameterName();
-    }
-    */
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String handleNotFound(ResourceNotFoundException ex)
@@ -27,7 +17,6 @@ public class ExceptionAndErrorController
         return ex.getMessage();
     }
     
-
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleUnauthorized(AuthenticationException ex)
